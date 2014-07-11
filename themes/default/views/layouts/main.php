749c9bb9940018e101700b05cb3720329871b3bd
mmmsharap <mmmsharap@gmail.com>
2014-07-11 10:32:32 +0600

    date_test

--- themes/default/views/layouts/main.php
+++ themes/default/views/layouts/main.php
@@ -7,8 +7,8 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 	
-	<link rel="shortcut icon" type="image/x-icon" href="http://www.taitandannika.com/library/images/icons/favicon.ico">
-	<link rel="apple-touch-icon" href="http://www.taitandannika.com/library/images/icons/apple-touch-icon.png">
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
 	
 	<link rel="stylesheet" href="<?=$this->getAssetsUrl();?>/w_files/styles.css" type="text/css" media="screen">
 	
