327d24fdad12a86856c9d29c46acd17b1cbaba7a
mmmsharap <mmmsharap@gmail.com>
2014-07-11 10:19:41 +0600

    vk appid

--- themes/default/views/layouts/main.php
+++ themes/default/views/layouts/main.php
@@ -30,7 +30,7 @@
 <script type="text/javascript" src="//vk.com/js/api/openapi.js?115"></script>
 
 <script type="text/javascript">
-  VK.init({apiId: 4455480, onlyWidgets: true});
+  VK.init({apiId: 4455514, onlyWidgets: true});
 </script>
 </head>
 
